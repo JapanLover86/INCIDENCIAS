b1a96a04502a6b1fc3c405083edef4867a2df0be
JapanLover86 <q01104k@upla.edu.pe>
2024-12-19 23:34:54 -0500

    Todo esta genial

---
+++ Control/wwwroot/Js/reportes.js
@@ -0,0 +1,5 @@
+﻿function generarReporte() {
+    // Aquí puedes crear un archivo, como un CSV o PDF, o simplemente abrir una URL con el reporte.
+    const reporteUrl = "/reporte-incidencias"; // Asumiendo que esta es la URL donde se genera el reporte
+    window.open(reporteUrl, '_blank');
+}
